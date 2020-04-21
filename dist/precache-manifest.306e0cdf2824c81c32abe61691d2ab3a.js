2fddc37f4728f601f509affce820a198c86ede43
alexislopes <alexisdrops@gmail.com>
2020-04-21 01:31:00 -0300

    fix(start sript): envia pasta dist e reescreve start script

---
+++ dist/precache-manifest.306e0cdf2824c81c32abe61691d2ab3a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c749df474ed7d86d9c6c",
+    "url": "/css/app.76870254.css"
+  },
+  {
+    "revision": "161704a84d77ecb4c826",
+    "url": "/css/chunk-adf3721e.d3e29190.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a1e867451991e1364be09856e8774da1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a1d06a35c283bc7ed5c",
+    "url": "/js/about.3ddd48db.js"
+  },
+  {
+    "revision": "c749df474ed7d86d9c6c",
+    "url": "/js/app.a8d71975.js"
+  },
+  {
+    "revision": "161704a84d77ecb4c826",
+    "url": "/js/chunk-adf3721e.7da5987b.js"
+  },
+  {
+    "revision": "45e13053ec1f58887475",
+    "url": "/js/chunk-vendors.ade13aea.js"
+  },
+  {
+    "revision": "ec04c6024736895c0418a80e8e32ced2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
